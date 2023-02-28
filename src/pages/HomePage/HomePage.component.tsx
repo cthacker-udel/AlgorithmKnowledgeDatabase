ec6fcb1b9e660c296f71b564885bd8be764e94fb
cthacker-udel <cthacker@udel.edu>
2023-02-28 04:16:45 -0500

    [main] Added greedy algos button

--- src/pages/HomePage/HomePage.component.tsx
+++ src/pages/HomePage/HomePage.component.tsx
@@ -11,37 +11,86 @@ import styles from "./HomePage.module.css";
  *
  * @returns - The homepage element, which is used to render the initial page that is displayed upon application startup
  */
-const HomePage: () => JSX.Element = (): JSX.Element => (
-    <div className={styles.home_page_main}>
-        <span className={styles.home_page_title}>
-            {"Algorithm Knowledge Database"}
-        </span>
-        <span className={styles.home_page_subtitles}>
-            <span className={styles.home_page_subtitle}>
-                {
-                    "Collection of the algorithms I've understood while learning Computer Science."
-                }
+const HomePage: () => JSX.Element = (): JSX.Element => {
+    const [rotateNodes, setRotateNodes] = React.useState<boolean>(false);
+    const [rotateGreedy, setRotateGreedy] = React.useState<boolean>(false);
+
+    return (
+        <div className={styles.home_page_main}>
+            <span className={styles.home_page_title}>
+                {"Algorithm Knowledge Database"}
             </span>
-            <span className={styles.home_page_subtitle}>
-                {"I will be adding algorithms to this page as I learn them!"}
+            <span className={styles.home_page_subtitles}>
+                <span className={styles.home_page_subtitle}>
+                    {
+                        "Collection of the algorithms I've understood while learning Computer Science."
+                    }
+                </span>
+                <span className={styles.home_page_subtitle}>
+                    {
+                        "I will be adding algorithms to this page as I learn them!"
+                    }
+                </span>
             </span>
-        </span>
-        <span className={styles.home_page_footer_title}>
-            {"Choose one of the sections below to learn more!"}
-        </span>
-        <div className={styles.home_page_footer_items}>
-            <OverlayTrigger
-                overlay={(properties: OverlayInjectedProps): JSX.Element =>
-                    renderTooltip(properties, "Graph Algorithms")
-                }
-                placement="bottom"
-            >
-                <div className={styles.home_page_footer_graph_algos}>
-                    <i className="fa-solid fa-circle-nodes fa-md" />
-                </div>
-            </OverlayTrigger>
+            <span className={styles.home_page_footer_title}>
+                {"Choose one of the sections below to learn more!"}
+            </span>
+            <div className={styles.home_page_footer_items}>
+                <span>
+                    <OverlayTrigger
+                        overlay={(
+                            properties: OverlayInjectedProps,
+                        ): JSX.Element =>
+                            renderTooltip(properties, "Graph Algorithms")
+                        }
+                        placement="bottom"
+                    >
+                        <div
+                            className={styles.home_page_footer_graph_algos}
+                            onMouseEnter={(): void => {
+                                setRotateNodes(true);
+                            }}
+                            onMouseLeave={(): void => {
+                                setRotateNodes(false);
+                            }}
+                        >
+                            <i
+                                className={`fa-solid fa-circle-nodes fa-md ${
+                                    rotateNodes ? "fa-flip" : ""
+                                }`}
+                            />
+                        </div>
+                    </OverlayTrigger>
+                </span>
+                <span>
+                    <OverlayTrigger
+                        overlay={(
+                            properties: OverlayInjectedProps,
+                        ): JSX.Element =>
+                            renderTooltip(properties, "Greedy Algorithms")
+                        }
+                        placement="bottom"
+                    >
+                        <div
+                            className={styles.home_page_footer_greedy_algos}
+                            onMouseEnter={(): void => {
+                                setRotateGreedy(true);
+                            }}
+                            onMouseLeave={(): void => {
+                                setRotateGreedy(false);
+                            }}
+                        >
+                            <i
+                                className={`fa-solid fa-money-bill fa-md ${
+                                    rotateGreedy ? "fa-shake" : ""
+                                }`}
+                            />
+                        </div>
+                    </OverlayTrigger>
+                </span>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default HomePage;
