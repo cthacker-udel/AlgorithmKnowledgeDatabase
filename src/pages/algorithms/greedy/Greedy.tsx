70feb52bac027c1a306f566605d4567e0bcd6368
cthacker-udel <cthacker@udel.edu>
2023-03-01 16:57:22 -0500

    [main] Added greedy algorithms page

---
+++ src/pages/algorithms/greedy/Greedy.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+/**
+ *
+ * @returns - The page containing all the greedy algorithms I have
+ */
+export const GreedyAlgorithms = (): JSX.Element => <div>{"Hello"}</div>;
